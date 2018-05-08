e89bbef2523cf7f70032c014b0d5a1370af28755
Naznin <nazninmalek111@gmail.com>
2018-05-08 18:10:00 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,6 +3,7 @@ var chocolateBars = ["snickers","hundred grand","kitkat","skittles"]
 function addElementToBeginningOfArray(element, chocolateBars)
 {
   chocolateBars.unshift(element)
+  return 
 }
 
-function 
+function destructivelyAddElementToBeginningOfArray
